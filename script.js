f0ee73dab6a367beee5d7d6c8daf21a2f46a0d05
Ayoub Jama Khalid <buuyacamaajdilaakh@gmail.com>
2024-09-08 17:04:05 +0300

    jawab

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Good Luck 💪🏾
+function brokenKeys(correct, typed) {
+    let broken = new Set();
+  
+    for (let i = 0; i < correct.length; i++) {
+      if (correct[i] !== typed[i]) {
+        broken.add(correct[i]);
+      }
+    }
+  
+    return [...broken];
+  }
+  
+  
+  console.log(brokenKeys("fikrcamp bootcamp", "fikrcomp boorcamp")); // ["a", "t"]
+  console.log(brokenKeys("good night", "gppd night")); // ["o"]
+  
